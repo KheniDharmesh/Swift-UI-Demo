96b5a1ec25407fe9734a099806aede41e33e2332
Dharmesh Kheni <dharmeshkheniios@gmail.com>
2020-04-30 23:11:57 +0530

    Initial Commit

---
+++ Swift UI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Demo
+//
+//  Created by Dharmesh Kheni on 30/04/20.
+//  Copyright © 2020 Dharmesh Kheni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
